bc643d997a449977d981cffd8fe70de5f03ebafa
emilio-robres <emiliorobres@gmail.com>
2021-05-26 11:07:33 +0200

    Modifico el componente princial App y renderizo Titulo, Espacio para chistes y un boton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const divStyle = {
+    margin: '80px',
+    height: '200px',
+    width: '80%',
+    background: 'grey',
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div >
+      <header >
+        <h1>
+          Chistes
+        </h1>
+        <div style={divStyle}>
+          
+        </div>
+        <button 
+          type="button" style={{width:'30 px'}}>Siguiente
+        </button>
       </header>
     </div>
   );
