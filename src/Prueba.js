d7b712faecca123a7d1c34eaf7c83dcb726fd9d3
emilio-robres <emiliorobres@gmail.com>
2021-06-07 20:19:17 +0200

    Subo Prueba

---
+++ src/Prueba.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Prueba() {
+    return (
+        <div className="card">
+            <div className="card-body">
+
+                <h4 className="card-title">Bienvenidos</h4>
+                <p className="card-text text-secondary">Pantalla de inicio de chistes para morirse de risa</p>
+            </div>
+
+        </div>
+    )
+};
+
+export default Prueba;
