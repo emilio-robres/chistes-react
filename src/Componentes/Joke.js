4c81ee402fa8c1c888f31ed171039ee7927393e5
emilio-robres <emiliorobres@gmail.com>
2021-06-09 23:36:56 +0200

    Intento del proyecto usando useContext

--- src/Componentes/Joke.js
+++ src/Componentes/Joke.js
@@ -1,21 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
    
-    
+import { DataContext } from "../Context/DataContext"; 
 
 
-function Joke (props) {
-
-      return(
-       
+function Joke () {
+  const {jokes}=useContext(DataContext);
+      
+  return(
         <div>
-  
-    <p style={{color:"black", fontSize:"20px", margin :"4git sts0px"
-
-    }}>{props.jokes}</p> 
-     
-
-        
-      </div>
-      );
-    }
+            <p style={{color:"black", fontSize:"20px", margin :"4git sts0px" }}>{jokes}</p> 
+        </div>
+  );
+}
 export default Joke;
